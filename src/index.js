6ca02c95f334b7d821cb5602ea4b1f00f029fa7c
Aleksei Ivanov <profpopoff@gmail.com>
2022-10-10 22:09:35 +0300

    Add media query for header

--- src/index.js
+++ src/index.js
@@ -5,3 +5,16 @@ import './gallery.html'
 import './projects.html'
 import './certificates.html'
 import './styles/index.scss'
+
+const header = document.querySelector('.primary-header')
+const nav = document.querySelector('.primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+document.querySelector('body').addEventListener('click', e => {
+   if (e.target == navToggle) {
+      const isVisible = header.getAttribute('data-visible')
+      header.setAttribute('data-visible', isVisible === "true" ? false : true)
+   } else if (e.target != nav.querySelector('ul')) {
+      header.setAttribute('data-visible', false)
+   }
+})
