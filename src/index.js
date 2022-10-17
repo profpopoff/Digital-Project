4a613612893a5f8b7b4599b1ba16f76654053b51
Aleksei Ivanov <profpopoff@gmail.com>
2022-10-17 19:49:42 +0300

    Add image slider

--- src/index.js
+++ src/index.js
@@ -19,3 +19,27 @@ document.querySelector('body').addEventListener('click', e => {
       header.setAttribute('data-visible', false)
    }
 })
+
+// slider
+const slides = document.getElementsByClassName("slider__slide")
+let currentSlide = 0
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+   changeSlide(currentSlide + 1)
+})
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+   changeSlide(currentSlide - 1)
+})
+
+function changeSlide(moveTo) {
+   if (moveTo >= slides.length) { moveTo = 0 }
+   if (moveTo < 0) { moveTo = slides.length - 1 }
+
+   slides[currentSlide].classList.toggle("active")
+   slides[moveTo].classList.toggle("active")
+
+   currentSlide = moveTo
+
+   document.getElementById("current-slide").innerHTML = `0${currentSlide + 1}`
+   document.getElementById("slides-length").innerHTML = `0${slides.length}`
+}
